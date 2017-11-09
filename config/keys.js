8caf7ea434c21796ffba5163312da52ceb8a2601
Puranam Pradeep Picasso <pichupicasso@gmail.com>
2017-11-09 22:40:28 +0530

    google Authentication to passport and router is made

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://userppp:userppp@ds149855.mlab.com:49855/storybooks-dev',
+    googleClientID:'265956497185-habsqvqjhe9a64n3i6egr30f1h6sn0ag.apps.googleusercontent.com',
+    googleClientSecret: 'UQEmyIDV3_bzhGLNlQVRwe-K'
+}
