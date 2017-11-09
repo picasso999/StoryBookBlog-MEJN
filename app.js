92738ee8831589e4c3ec3728a250fad7fe62a66b
Puranam Pradeep Picasso <pichupicasso@gmail.com>
2017-11-10 02:42:46 +0530

    basic UI setup with login entangled and sideNav bar

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+
+//Load User Model
+require('./models/User');
 
 //Passport Config
 require('./config/passport')(passport);
 
-
-//Lod Routes
+//Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
 
+//Load keys
+const keys = require('./config/keys');
+//Map global promises
+mongoose.Promise = global.Promise;
+//Mongoose connect
+mongoose.connect(keys.mongoURI, {
+    useMongoClient:true
+}).then(()=>console.log('MongoDB connected')).catch(err => console.log(err));
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('It works!');
-});
+//Handlebars middleware
+app.engine('handlebars',exphbs({
+    defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
+app.use(session({
+    secret:'secret',
+    resave:false,
+    saveUninitialized:false
+}))
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//set golbal vars
+app.use((req,res,next)=>{
+    res.locals.user = req.user ||null;
+    next();
+})
 
 //Use Routes
+app.use('/',index);
 app.use('/auth',auth);
 
 const port = process.env.PORT || 5000;
